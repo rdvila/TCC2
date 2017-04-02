27b0b9d95b9475c8a99094fad02d332ba131e317
Rodrigo D'avila <r0dra42@gmail.com>
2017-04-02 10:56:35 -0300

    ajustado script de conversão

--- parse_svg.py
+++ parse_svg.py
@@ -16,27 +16,32 @@ tree = ET.parse(inputfile)
 root = tree.getroot()
 paths = root.findall('./svg:g/svg:path', ns)
 
+SVG1 = '''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg    xmlns:dc="http://purl.org/dc/elements/1.1/"   xmlns:cc="http://creativecommons.org/ns#"   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"   xmlns:svg="http://www.w3.org/2000/svg"   xmlns="http://www.w3.org/2000/svg"    xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" width="500" height="500">
+<g>
+'''
+
+SVG2 = '''  </g>
+</svg>
+'''
+
 svgpath1 = '''<path
        style="fill:none;fill-rule:evenodd;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
        d="'''
 svgpath2 = ''' Z"/>
 '''
 
+print(SVG1)
 for p in paths:
 	new_path = Path()
 	path     = parse_path(p.attrib["d"])
-	for sp in path:
-		name = type(sp).__name__
-		if name == 'Line':
-			new_path.append(sp)
+	start    = None
+	for px in list(frange(0, 1, decimal.Decimal('0.005'))):
+		if start is None:
+			start = path.point(float(px))
 		else:
-			start = None
-			values = list(frange(0, 1, decimal.Decimal('0.01')))
-			for px in values:
-				if start is None:
-					start = sp.point(float(px))
-				else:
-					end   = sp.point(float(px))
-					new_path.append(Line(start, end))
-					start = end
+			end   = path.point(float(px))
+			new_path.append(Line(start, end))
+			start = end
 	print(svgpath1 + new_path.d() + svgpath2)
+print(SVG2)
