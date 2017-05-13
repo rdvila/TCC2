277b827149aaceb48961e2b5ab8554e63dab6e52
Rodrigo D'avila <r0dra42@gmail.com>
2017-05-13 15:39:08 -0300

    adicionado novos testes

---
+++ tests/teste7/start-tabu.sh
@@ -0,0 +1,15 @@
+"/c/Program Files (x86)/Java/jdk1.8.0_121/bin/java" -Xmx1024m -Xms256m -cp "packing.jar;jenetics.jar;cc.jar" TabuExecutor fu-converted 100 1 60000
+"/c/Program Files (x86)/Java/jdk1.8.0_121/bin/java" -Xmx1024m -Xms256m -cp "packing.jar;jenetics.jar;cc.jar" TabuExecutor fu-converted 100 1 120000
+"/c/Program Files (x86)/Java/jdk1.8.0_121/bin/java" -Xmx1024m -Xms256m -cp "packing.jar;jenetics.jar;cc.jar" TabuExecutor fu-converted 100 1 300000
+"/c/Program Files (x86)/Java/jdk1.8.0_121/bin/java" -Xmx1024m -Xms256m -cp "packing.jar;jenetics.jar;cc.jar" TabuExecutor poly1a-converted 100 1 60000
+"/c/Program Files (x86)/Java/jdk1.8.0_121/bin/java" -Xmx1024m -Xms256m -cp "packing.jar;jenetics.jar;cc.jar" TabuExecutor poly1a-converted 100 1 120000
+"/c/Program Files (x86)/Java/jdk1.8.0_121/bin/java" -Xmx1024m -Xms256m -cp "packing.jar;jenetics.jar;cc.jar" TabuExecutor poly1a-converted 100 1 300000
+"/c/Program Files (x86)/Java/jdk1.8.0_121/bin/java" -Xmx1024m -Xms256m -cp "packing.jar;jenetics.jar;cc.jar" TabuExecutor poly2b-converted 100 1 60000
+"/c/Program Files (x86)/Java/jdk1.8.0_121/bin/java" -Xmx1024m -Xms256m -cp "packing.jar;jenetics.jar;cc.jar" TabuExecutor poly2b-converted 100 1 120000
+"/c/Program Files (x86)/Java/jdk1.8.0_121/bin/java" -Xmx1024m -Xms256m -cp "packing.jar;jenetics.jar;cc.jar" TabuExecutor poly2b-converted 100 1 300000
+"/c/Program Files (x86)/Java/jdk1.8.0_121/bin/java" -Xmx1024m -Xms256m -cp "packing.jar;jenetics.jar;cc.jar" TabuExecutor poly3b-converted 100 1 60000
+"/c/Program Files (x86)/Java/jdk1.8.0_121/bin/java" -Xmx1024m -Xms256m -cp "packing.jar;jenetics.jar;cc.jar" TabuExecutor poly3b-converted 100 1 120000
+"/c/Program Files (x86)/Java/jdk1.8.0_121/bin/java" -Xmx1024m -Xms256m -cp "packing.jar;jenetics.jar;cc.jar" TabuExecutor poly3b-converted 100 1 300000
+"/c/Program Files (x86)/Java/jdk1.8.0_121/bin/java" -Xmx1024m -Xms256m -cp "packing.jar;jenetics.jar;cc.jar" TabuExecutor poly4b-converted 100 1 60000
+"/c/Program Files (x86)/Java/jdk1.8.0_121/bin/java" -Xmx1024m -Xms256m -cp "packing.jar;jenetics.jar;cc.jar" TabuExecutor poly4b-converted 100 1 120000
+"/c/Program Files (x86)/Java/jdk1.8.0_121/bin/java" -Xmx1024m -Xms256m -cp "packing.jar;jenetics.jar;cc.jar" TabuExecutor poly4b-converted 100 1 300000
